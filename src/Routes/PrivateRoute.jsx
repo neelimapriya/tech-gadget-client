982c064c40039b2acc071c41e393f68c67e1f318
neelimapriya <neelimasultana6@gmail.com>
2023-11-27 00:45:55 +0600

    add react loader

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
+import { HashLoader } from "react-spinners";
 
 const PrivateRoute = ({children}) => {
     const {user, loading}=useAuth()
     const location =useLocation()
     if(loading){
-        return <div className="flex justify-center items-center mt-40"><progress className="progress w-56"></progress></div>
+        return <div className="flex justify-center items-center mt-40"><HashLoader size={100} color="black"></HashLoader></div>
     }
     if(user){
         return children
