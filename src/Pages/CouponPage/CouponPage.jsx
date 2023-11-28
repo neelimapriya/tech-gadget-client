da65e195d4fef460d34e3d46584575db50a12cae
neelimapriya <neelimasultana6@gmail.com>
2023-11-28 21:45:34 +0600

    setup coupon page

--- src/Pages/CouponPage/CouponPage.jsx
+++ src/Pages/CouponPage/CouponPage.jsx
@@ -1,11 +1,15 @@
 const CouponPage = () => {
   return (
-    <div>
-      <div className="mockup-phone">
-        <div className="camera"></div>
-        <div className="display">
-          <div className="artboard artboard-demo phone-1">Coupon.</div>
-        </div>
+    <div className="bg-gradient-to-r from-pink-800 to-pink-950 text-white  flex pt-10 px-5 pb-10">
+     <div>
+      <h2 className="text-3xl font-bold text-center mt-5 font-serif">Use our coupon code to get 20$ of when purchase membership</h2>
+    <div className="flex rounded-lg justify-center items-center mt-3 text-center gap-2 ">
+    <p className="text-center text-lg">Use Promo Code </p>
+    <p className="text-2xl font-bold bg-gradient-to-r from-green-700 to-yellow-500 p-2 rounded-lg animate-pulse">TG20</p>
+    </div>
+     </div>
+      <div>
+        <img src="https://i.ibb.co/gr8Mbvb/img-2.png" alt="" />
       </div>
     </div>
   );
